da75fa19bdd9b81ad62756a545dcf07154e3197a
Atanazio01 <marcosd.atanazio@gmail.com>
2026-06-21 10:56:59 -0300

    Data validation with FluentValidation in .NET

--- src/Backend/MyRecipeBook.Api/Controllers/UsersController.cs
+++ src/Backend/MyRecipeBook.Api/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Application.UseCases.User.Register;
 using MyRecipeBook.Communication.Requests;
 
 namespace MyRecipeBook.Api.Controllers;
@@ -10,7 +11,9 @@ public class UsersController : ControllerBase
     [HttpPost]
     public IActionResult Register([FromBody] RequestRegisterUserAccountJson request)
     {
-        //Registro a conta de uma pessoa
+        var useCase = new RegisterUserAccountUseCase();
+
+        useCase.Execute(request);
 
         return Created();
     }
---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipeBook.Application.UseCases.User.Register;
+
+public class RegisterUserAccountValidator : AbstractValidator<RequestRegisterUserAccountJson>
+{
+    public RegisterUserAccountValidator()
+    {
+        RuleFor(user => user.Name).NotEmpty().WithMessage("O nome é obrigatório.");
+        RuleFor(user => user.Email).NotEmpty().WithMessage("O email é obrigatório.");
+        RuleFor(user => user.Password).NotEmpty().WithMessage("A senha é obrigatória.");
+        When(user => string.IsNullOrWhiteSpace(user.Email) == false, () =>
+        {
+            RuleFor(user => user.Email).EmailAddress().WithMessage("O email deve ser válido.");
+        });
+    }
+}
