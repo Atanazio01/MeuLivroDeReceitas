20ae867af64924fbb7f789090492bd8f4d87001e
Atanazio01 <marcosd.atanazio@gmail.com>
2026-06-24 09:14:11 -0300

    Using Resource Files in .NET to Manage Application Text

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exception;
 
 namespace MyRecipeBook.Application.UseCases.User.Register;
 
@@ -7,9 +8,9 @@ public class RegisterUserAccountValidator : AbstractValidator<RequestRegisterUse
 {
     public RegisterUserAccountValidator()
     {
-        RuleFor(user => user.Name).NotEmpty().WithMessage("O nome é obrigatório.");
-        RuleFor(user => user.Email).NotEmpty().WithMessage("O email é obrigatório.");
-        RuleFor(user => user.Password).NotEmpty().WithMessage("A senha é obrigatória.");
+        RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesException.VALIDATION_NAME_REQUIRED);
+        RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessagesException.VALIDATION_EMAIL_REQUIRED);
+        RuleFor(user => user.Password).NotEmpty().WithMessage(ResourceMessagesException.VALIDATION_PASSWORD_REQUIRED);
         When(user => string.IsNullOrWhiteSpace(user.Email) == false, () =>
         {
             RuleFor(user => user.Email).EmailAddress().WithMessage("O email deve ser válido.");
