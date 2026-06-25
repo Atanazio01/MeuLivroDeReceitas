b4370a47c64dc127d628bc14c052e4748f2a3bed
Atanazio01 <marcosd.atanazio@gmail.com>
2026-06-25 14:43:51 -0300

    feat: Criando Exceptions Customizadas na API .NET

---
+++ src/Shared/MyRecipeBook.Exception/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,10 @@
+﻿namespace MyRecipeBook.Exception.ExceptionsBase;
+
+public class ErrorOnValidationException: MyRecipeBookException
+{
+    private readonly List<string> _errorMessages;
+    public ErrorOnValidationException(List<string> errorMessages)
+    {
+        _errorMessages = errorMessages;
+    }
+}
